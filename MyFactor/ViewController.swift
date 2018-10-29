32c113f99349aa9d70a43ece6fa0b37f129a67a1
masterUNG <phrombutr@gmail.com>
2018-10-29 13:41:41 +0700

    Calculate Success

--- MyFactor/ViewController.swift
+++ MyFactor/ViewController.swift
@@ -9,7 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    Explicit
+    var numberString: String = ""
+    var mainNumberInts = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    
+    
 
+    
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+//        Get Value From TextField
+        numberString = numberTextField.text!
+        print("numberString ==> \(numberString)")
+        
+//        Calculate
+        var answerStrings = [String]()
+        var answerInt = 0
+        var resultString = ""
+        
+        
+        for myResultInt in mainNumberInts {
+            
+            answerInt = myResultInt * Int(numberString)!
+            print("answerInt ==> \(answerInt)")
+            resultString = numberString + " x " + String(myResultInt) + " = " + String(answerInt)
+            print("resultString ==> \(resultString)")
+            
+            answerStrings.append(resultString)
+            
+        }   // for
+        
+        print("answerStrings ==> \(answerStrings)")
+        
+        
+        
+    }   // calculate Function
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
